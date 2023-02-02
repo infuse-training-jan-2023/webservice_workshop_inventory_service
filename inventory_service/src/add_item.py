77f3406c3cec643dbfa75b942901f8f07c7b23d4
Leon <mca.2016@unigoa.com>
2023-02-02 16:44:15 +0530

    updated delete container.py

--- inventory_service/src/add_item.py
+++ inventory_service/src/add_item.py
@@ -2,7 +2,7 @@
 def add_item(cursor, container_name , item):
     try:       
         insert_cursor = cursor.execute('insert into inventory(container_name, item) values(?,?)', (container_name, item ))
-       
+        
         return {
             'id': insert_cursor.lastrowid,
             'container_name':container_name,
--- inventory_service/src/delete_container.py
+++ inventory_service/src/delete_container.py
@@ -1,8 +1,6 @@
 def delete_container(cursor, container_name):
     try:  
         print(type(container_name))     
-        first = container_name[0]
-        print(first)
-        return  cursor.execute('DELETE FROM inventory WHERE container_name like "?%"',(first))
+        return  cursor.execute("DELETE FROM inventory WHERE container_name = ? ",(container_name,))
     except Exception as e:
       raise Exception('Error: ', e)
